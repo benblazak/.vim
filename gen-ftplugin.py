3bd690c1243538572a451b56d35bce55eea5ffcc
ben <ben>
2008-04-21 05:35:27 +0000

    changed the test to see wether or not this file should indeed be sourced - ..don't think i did anything else

--- gen-ftplugin.py
+++ gen-ftplugin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # ben blazak -- 2008-04-20
-# $Id: gen-ftplugin.py,v 1.1 2008-04-21 00:31:02 ben Exp $
+# $Id: gen-ftplugin.py,v 1.2 2008-04-21 05:35:27 ben Exp $
 
 # a little script to generate ftplugin files from vimscript files where every
 # non-comment line is preceded by "[filetype list]\s*++\s*"
@@ -26,8 +26,21 @@ for wholeline in open('ftplugin.genfile', 'r'):
 			files[type] += line
 		else:
 			files[type] = '" generated from ftplugin.genfile -- ' \
-					+ str(datetime.datetime.now()) + '\n\n' + line
+				+ str(datetime.datetime.now()) + '\n\n' \
+				+ 'if &filetype != "' + type \
+				+ '" || exists("b:did_ftplugin")\n' \
+				+ '\tfinish\n' \
+				+ 'endif\n\n' \
+				+ line
 
 for type in files.keys():
 	open('ftplugin_ben/' + type + '_ben.vim', 'w').write(files[type])
+
+# notes:
+#
+# contents of file enclosed in an 'if ...' to protect setting from getting
+# overridden by other settings, as in the case where a php ftplugin (that i
+# didn't write) sources something to do with html, which sources my html
+# ftplugin, which overrides @c and @r -- yup
+
 
